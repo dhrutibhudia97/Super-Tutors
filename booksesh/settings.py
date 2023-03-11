8c49b809157601811914a8b3ab63a055119b65a7
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-03-11 12:50:09 +0000

    add booking-tutor-sessions to installed apps in setting.py file

--- booksesh/settings.py
+++ booksesh/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['booksesh.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['booksesh.herokuapp.com', 'localhost', 'booking-tutor-sessions.herokuapp.com']
 
 
 # Application definition
