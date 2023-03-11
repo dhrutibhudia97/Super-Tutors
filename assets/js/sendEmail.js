d4ae22bc664e8c782e2705e158207a33508cfb5c
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-03-11 00:45:10 +0000

    Connecting email to contac.html page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,15 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "Super Tutor", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "from_querysubmission": contactForm.querysubmission.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error)
+        });
+        return false; 
+}
