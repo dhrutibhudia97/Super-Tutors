1ca7f443be82c4fe7fe4193b79fe5b26dc5b92f8
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-03-13 21:38:33 +0000

    Create mode session class in models.py

--- booksesh/bookings/models.py
+++ booksesh/bookings/models.py
@@ -6,3 +6,17 @@ from datetime import datetime
 
 
 # Create your models here.
+
+class Session(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
+    tutor_service = models.CharField(choices=SERVICE_OPTIONS)
+    day = models.DateTimeField(default=datetime.now)
+    time = models.CharField(choices=TIME_OPTIONS)
+    ordered_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-day']
+
+    def __str__(self):
+        return f"Tutor Service: {self.tutor_service} | date: {self.day} | Time: {self.time}"
