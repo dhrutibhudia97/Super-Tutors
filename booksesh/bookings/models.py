54a8e18298f2a686ac503acd614ab6d944098ea0
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-03-13 21:57:03 +0000

    Add choices for serive options and time options

--- booksesh/bookings/models.py
+++ booksesh/bookings/models.py
@@ -1,11 +1,25 @@
 from django.db import models
-
 from django.contrib.auth.models import User
-
 from datetime import datetime
 
+# same service 3 times, as up to 3 people can book the same group session
+SERVICE_OPTIONS = (
+    ("GCSE Biology", "GCSE Biology", "GCSE Biology"),
+    ("GCSE Chemistry", "GCSE Chemistry", "GCSE Chemistry"),
+    ("GCSE Physics", "GCSE Physics", "GCSE Physics"),
+)
+
+# 9 options for each time zone, so each group has max 3 people in it for 3 types of services
+TIME_OPTIONS = (
+    ("4-5pm", "4-5pm", "4-5pm", "4-5pm", "4-5pm", "4-5pm", "4-5pm", "4-5pm", "4-5pm"),
+    ("5-6pm", "5-6pm", "5-6pm", "5-6pm", "5-6pm", "5-6pm", "5-6pm", "5-6pm", "5-6pm"),
+    ("6-7pm", "6-7pm", "6-7pm", "6-7pm", "6-7pm", "6-7pm", "6-7pm", "6-7pm", "6-7pm"),
+    ("7-8pm", "7-8pm", "7-8pm", "7-8pm", "7-8pm", "7-8pm", "7-8pm", "7-8pm", "7-8pm"),
+    ("8-9pm", "8-9pm", "8-9pm", "8-9pm", "8-9pm", "8-9pm", "8-9pm", "8-9pm", "8-9pm"),
+)
 
 # Create your models here.
+
 
 class Session(models.Model):
     user = models.ForeignKey(
