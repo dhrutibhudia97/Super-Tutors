a5458daa46bdcfe4cb254ab771e87c8af2d03daa
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-03-15 15:39:27 +0000

    deleted old project and app and installed new project bookingsessions and new app called bookings

--- bookingsessions/asgi.py
+++ bookingsessions/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for booksesh project.
+ASGI config for bookingsessions project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booksesh.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookingsessions.settings')
 
 application = get_asgi_application()
--- bookingsessions/wsgi.py
+++ bookingsessions/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for booksesh project.
+WSGI config for bookingsessions project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booksesh.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookingsessions.settings')
 
 application = get_wsgi_application()
