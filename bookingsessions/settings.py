af97b93a4f8bf93dc698e2d7ac397d49ca4dd585
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-03-15 16:03:42 +0000

    added booking-tutor-sessions.herokuapp.com to allowed hosts

--- bookingsessions/settings.py
+++ bookingsessions/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['booking-tutor-sesions.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['booking-tutor-sessions.herokuapp.com', 'localhost']
 
 
 # Application definition
