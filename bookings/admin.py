1b220287b2db3fca84ca94ed45f391df96143388
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-03-16 20:06:49 +0000

    add more features to class PostAdmin and commented out

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-
 # from .models import Post 
-
 # from django_summernote.admin import SummernoteModelAdmin
 
 # @admin.register(Post)
 # class PostAdmin(SummernoteModelAdmin):
 #     prepopulated_fields = {'slug': ('title',)}
 #     summernote_fields = ('content')
-
+#     list_filter = ('status', 'created_on')
+#     summernote_fields = ('content')
