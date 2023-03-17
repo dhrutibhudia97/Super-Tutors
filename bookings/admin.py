35c2bd33cbb98bf9f9b6508a7b332e31ccabfb44
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-03-17 10:41:36 +0000

    create post admin and created and commented out comment admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
-from .models import Post 
+from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
 
 
-# @admin.register(Post)
-# class PostAdmin(SummernoteModelAdmin):
-#     prepopulated_fields = {'slug': ('title',)}
-#     summernote_fields = ('content')
-#     list_filter = ('status', 'created_on')
-#     list_display = ('title', 'slug', 'status', 'created_on')
+@admin.register(Post)  # registers both post and post admin class with admin site
+class PostAdmin(SummernoteModelAdmin):
+   
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content')
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+
+
+# @admin.register(Comment)
+# class CommentAdmin(admin.ModelAdmin):
+    
+#     list_display = ('name', 'body', 'post', 'created_on', 'approved')
+#     list_filter = ('approved', 'created_on')
+#     search_fields = ('name', 'email', 'body')
+#     actions = ['approve_comments']
+
+#     def approve_comments(self, request, queryset):
+#         queryset.update(approved=True)
 
