6a36aef87b7dd51fb27dacd027b0fcbeda4ba604
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-03-17 12:19:05 +0000

    Created view for PostList in views.py

--- bookings/models.py
+++ bookings/models.py
@@ -7,6 +7,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 
+
 class Post(models.Model):
     title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -17,7 +18,7 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    # likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
 
     class Meta:
         ordering = ['-created_on']
