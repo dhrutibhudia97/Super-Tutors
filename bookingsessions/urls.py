09b35f02ecc07a4d1a5d3bc066f9d3013ae4bb5b
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-03-18 10:49:09 +0000

    configure both urls.py files

--- bookingsessions/urls.py
+++ bookingsessions/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),
-    path('theblog.html', include('theblog.urls'), name='theblog_urls'),
+    # path('summernote/', include('django_summernote.urls')),
+    path('', include('bookings.urls')),
 ]
