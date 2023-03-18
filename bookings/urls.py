dad8d8f95a47f77a1a4d46a06f27c8cbe7869554
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-03-18 23:25:19 +0000

    add urls to urls.py in bookings folder

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('book-session', views.booksession, name='booksession'),
+    path('session-submit', views.sessionsubmit, name='sessionsubmit'),
+    path('user-View', views.userView, name='userView'),
+    path('user-update-view/<int:id>', views.userUpdateView, name='userUpdateView'),
+    path('session-update-submit/<int:id>', views.sessionUpdateSubmit, name='sessionUpdateSubmit'),
+    path('staff-View', views.staffView, name='staffView'),
 ]
