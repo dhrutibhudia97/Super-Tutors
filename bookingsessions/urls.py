63590503fc8c14ad0e1cba0988133a770ac7178c
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-03-18 22:55:34 +0000

    uadd url patterns in urls.py file

--- bookingsessions/urls.py
+++ bookingsessions/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     # path('summernote/', include('django_summernote.urls')),
-    path('', include('bookings.urls')),
+    # path('', include('bookings.urls')),
+    path('', views.index, name='index'),
+    path('book-session', views.booksession, name='booksession'),
+    path('session-submit', views.sessionsubmit, name='sessionsubmit'),
+    path('user-View', views.userView, name='userView'),
+    path('user-update-view/<int:id>', views.userUpdateView, name='userUpdateView'),
+    path('session-update-submit/<int:id>', views.sessionUpdateSubmit, name='sessionUpdateSubmit'),
+    path('staff-View', views.staffView, name='staffView'),
 ]
