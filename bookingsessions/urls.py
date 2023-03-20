3fcc13456d50f926da7b0cf80b5d814cb1ea0103
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-03-20 18:13:59 +0000

    add all auth apps to installed apps in settings.py file

--- bookingsessions/urls.py
+++ bookingsessions/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     # path('summernote/', include('django_summernote.urls')),
     # path("", include('bookings.urls')),
     # path('', views.index, name='index'),
