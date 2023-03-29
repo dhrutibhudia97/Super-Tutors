4c3dbf83dd98040007e2eb64e650671c2cc51cb1
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-03-29 18:03:29 +0000

    commented out messages tags in settings.py

--- bookingsessions/settings.py
+++ bookingsessions/settings.py
@@ -52,13 +52,13 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 
-MESSAGE_TAGS = {
-        messages.DEBUG: 'alert-info',
-        messages.INFO: 'alert-info',
-        messages.SUCCESS: 'alert-success',
-        messages.WARNING: 'alert-warning',
-        messages.ERROR: 'alert-danger',
-    }
+# MESSAGE_TAGS = {
+#         # messages.DEBUG: 'alert-info',
+#         # messages.INFO: '',
+#         messages.SUCCESS: 'alert-success',
+#         messages.WARNING: 'alert-warning',
+#         messages.ERROR: 'alert-danger',
+#     }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
